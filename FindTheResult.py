1e665400e5737bd7abef3c1894a3a52d1c953303
EvaDM <evamorante1969@gmail.com>
2023-04-09 15:07:32 +0200

    next exercice on if - else

---
+++ FindTheResult.py
@@ -0,0 +1,8 @@
+correct_answer = 9
+user_input = input("what is the result of 2 + 7 : ")
+if user_input == str(correct_answer):
+    print("Congratulations, you win !")
+else :
+    print("Try it again !")
+
+print("The game continues.")
