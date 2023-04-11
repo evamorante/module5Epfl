002cee21a19fabe6006416acda562d8a1a045a14
EvaDM <evamorante1969@gmail.com>
2023-04-11 18:48:09 +0200

    trying something...hhhh

--- SecretWord2.py
+++ SecretWord2.py
@@ -2,4 +2,4 @@ secret_word = "Heyyy"
 user_guess = input("Try to guess the secret word :")
 
 if secret_word == user_guess:
-    print("You find the secret word !")
+  print("You find the secret word !")
