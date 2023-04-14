87ed1cc1896dd1e12fdb2e9f746c3b1ab34f28bf
EvaDM <evamorante1969@gmail.com>
2023-04-14 16:34:38 +0200

    correction erreur d'espace

--- SecretWord2.py
+++ SecretWord2.py
@@ -1,5 +1,8 @@
+#exercise 10. - you find the secret word
 secret_word = "Heyyy"
-user_guess = input("Try to guess the secret word :")
+user_guess = input("Try to guess the secret word : ")
+print(secret_word)
+print(user_guess)
 
 if secret_word == user_guess:
   print("You find the secret word !")
