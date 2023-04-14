89f154c8a10c1065368830c4b7eeadd0ebec3728
EvaDM <evamorante1969@gmail.com>
2023-04-14 17:03:08 +0200

    code for fullConverter with if/else condition

---
+++ fullConverterWithIfElseCond.py
@@ -0,0 +1,25 @@
+def changeMoney(dollars):
+    francs = dollars*1.02
+    message = str(dollars)+" dollars are "+str(francs)+" francs."
+    return message
+
+print(changeMoney(25))
+if int(francs) >=100:
+    print("You are super rich")
+
+
+
+def changeWater(liters):
+    gallons = liters*0.17
+    message = str(liters)+" liters are "+str(gallons)+" gallons."
+    return message
+
+print(changeWater(3))
+
+def changeHeigth(meters):
+    centimeters = meters*100
+    message = str(centimeters)+" centimeters are "+str(meters)+" meters."
+    return message
+
+print(changeHeigth(34))
+
