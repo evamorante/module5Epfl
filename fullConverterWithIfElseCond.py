62dfcf7acde7fce8102b3ac5ea13022b28f5ad0e
EvaDM <evamorante1969@gmail.com>
2023-04-14 17:14:54 +0200

    testing more and more

--- fullConverterWithIfElseCond.py
+++ fullConverterWithIfElseCond.py
@@ -13,16 +13,21 @@ print(changeMoney(102))
 
 
 def changeWater(liters):
+    if liters >100:
+        print("Pay attention at the energie consommation")
+
     gallons = liters*0.17
     message = str(liters)+" liters are "+str(gallons)+" gallons."
     return message
 
-print(changeWater(3))
+print(changeWater(102))
 
 def changeHeigth(meters):
+    if meters >100:
+        print("That's very long")
     centimeters = meters*100
     message = str(centimeters)+" centimeters are "+str(meters)+" meters."
     return message
 
-print(changeHeigth(34))
+print(changeHeigth(200))
 
