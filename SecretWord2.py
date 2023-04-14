fbbcf355c98bba90b0fd30213fc7341e71314353
EvaDM <evamorante1969@gmail.com>
2023-04-14 17:22:46 +0200

    creating the 3rd round of guess the secret word with if/else cond

--- SecretWord2.py
+++ SecretWord2.py
@@ -3,6 +3,8 @@ secret_word = "Heyyy"
 user_guess = input("Try to guess the secret word : ")
 print(secret_word)
 print(user_guess)
+#print(secret_word) - if not working properly, do the print test
+#print(user_guess) - with the different variables
 
 if secret_word == user_guess:
   print("You find the secret word !")
