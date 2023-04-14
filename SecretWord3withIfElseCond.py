9e9d1860c4d07bd7fbf51fdec879dda7eac5b0e7
EvaDM <evamorante1969@gmail.com>
2023-04-14 17:27:50 +0200

    creating file for the 3rd part of the guess secret word game

---
+++ SecretWord3withIfElseCond.py
@@ -0,0 +1,11 @@
+#exercise 18. - you find the secret word - with if condition
+secret_word = "Heyyy"
+user_guess = input("Try to guess the secret word : ")
+
+
+if secret_word == user_guess:
+  print("You find the secret word !")
+else:
+  print("You have to try one more time !")
+
+print("Never mind you find or not, this game is funny")
