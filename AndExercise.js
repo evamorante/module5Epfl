2fa8c44c070c4c48ed0df7202d0fd5bc34f49674
EvaDM <evamorante1969@gmail.com>
2023-04-15 17:26:32 +0200

    html file to link with if/else/and condition file in JS

--- AndExercise.js
+++ AndExercise.js
@@ -9,3 +9,26 @@ If it is sunny and if it is warm, take sunglasses and a t-shirt.
 If it is anything else, stay home!
 
 Can you translate this into a Python program? When you are done, translate it to JavaScript as well! */
+
+var weatherInput = window.prompt("Is it today raining or sunny ? : ")
+var temperatureInput = window.prompt("Is it today warm or cold ? : ")
+
+if (weatherInput == "raining" && temperatureInput == "cold"){
+    console.log("Take an umbrella and a jacket");
+}
+else if (weatherInput == "raining" && temperatureInput == "warm"){
+    console.log("Take an umbrella and a t-shirt");
+}
+  
+else if (weatherInput == "sunny" && temperatureInput == "cold"){
+    print("Take sunglasses and a jacket");
+}
+  
+else if (weatherInput == "sunny" && temperatureInput == "warm"){
+    console.log("Take sunglasses and a t-shirt");
+}
+  
+else{
+    print("Stay home !");
+}
+  
