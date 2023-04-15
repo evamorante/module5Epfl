f8d539e13b2ad6ef83fa0074fed6f9da80f00cfe
EvaDM <evamorante1969@gmail.com>
2023-04-15 17:07:59 +0200

    trying doing something hhhhh

---
+++ AndExercise.js
@@ -0,0 +1,11 @@
+// exercise 26. in javascript
+
+/**Look at the following program written in plain English:
+
+If it is raining and if it is cold, take an umbrella and a warm jacket.
+If it is raining and if it is warm, take an umbrella and a t-shirt.
+If it is sunny and if it is cold, take sunglasses and a warm jacket.
+If it is sunny and if it is warm, take sunglasses and a t-shirt.
+If it is anything else, stay home!
+
+Can you translate this into a Python program? When you are done, translate it to JavaScript as well! */
