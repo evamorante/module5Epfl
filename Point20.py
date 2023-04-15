d92427a0fa395b78e29a16a3510416ec3851ad4b
EvaDM <evamorante1969@gmail.com>
2023-04-15 10:06:56 +0200

    testing and testing and testing again...hhhhh

---
+++ Point20.py
@@ -0,0 +1,31 @@
+# Ask the user to enter a first number.
+#Ask the user to enter a second number.
+#Subtract the first number from the second one.
+#If the result is bigger than 10, then display The result is greater than 10.
+#If the result is bigger than 0 (but not bigger than 10!), display The result is greater than 0 but not than 10.
+#If the result is 0, display The result is zero.
+#For any other result, display The result is a negative number.
+#Whatever happens, display You can try again! just before the program exits.
+
+def substraction():
+    
+    number_one = input("Enter a number : ")
+    number_two = input("Enter a second number : ")
+    result = int(number_one) - int(number_two)
+    return result
+
+
+print(substraction())
+
+
+if  >10:
+    print("The result is bigger than 10")
+elif substraction <10:
+    print("The result is between 0 and 10")
+elif substraction == 0:
+    print("The result is zero")
+else:
+    print("The result is not available")
+
+print("You can try again !")
+
