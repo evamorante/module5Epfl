34d0c1e98c76bf46741898c4b19a8f77c27305f6
EvaDM <evamorante1969@gmail.com>
2023-04-15 10:45:36 +0200

    testing if/else cond

--- Point21.js
+++ Point21.js
@@ -6,8 +6,24 @@ If the result is 0, display The result is zero.
 For any other result, display The result is a negative number.
 Whatever happens, display You can try again! just before the program exits. */
 
-var numberOne = 19;
+var numberOne = 23;
 var numberTwo = 12;
 var result = numberOne-numberTwo;
+
 console.log(result);
+
+if (result >10){
+    console.log("The result is greater than 10");
+}
+else if (result >0){
+    console.log("The result is between 0 and 10");
+}
+else if (result == 0){
+    console.log("The result is zero");
+}
+else {
+    console.log("The result is unavailable");
+}
+
+
 
