decb747e8532ca13a93b57796926938d38751058
EvaDM <evamorante1969@gmail.com>
2023-04-15 10:36:18 +0200

    adding Point21.js to the repository

---
+++ Point21.js
@@ -0,0 +1,13 @@
+/**Define and assign two number variables.
+Subtract the first number from the second one.
+If the result is bigger than 10, then display The result is greater than 10.
+If the result is bigger than 0 (but not bigger than 10!), display The result is greater than 0 but not than 10.
+If the result is 0, display The result is zero.
+For any other result, display The result is a negative number.
+Whatever happens, display You can try again! just before the program exits. */
+
+var numberOne = 19;
+var numberTwo = 12;
+var result = numberOne-numberTwo;
+console.log(result);
+
