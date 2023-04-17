aeb7920f5d8b011e819e2ffbbf254b90e3dcb866
EvaDM <evamorante1969@gmail.com>
2023-04-17 09:40:29 +0200

    last corrections - and checks - ok

--- And&OrExercise.py
+++ And&OrExercise.py
@@ -12,13 +12,13 @@ if python or javascript:
     print("this is a good course")
 if python and javascript:
     print("this is a TTC course")
+if number >10 or color == "blue":
+    print("the test is true")
+if number <10 and color =="blue":
+    print("the test is True")
+
+# pourquoi le print() -- output --> True or False ne marche pas avec if/and/or condition ?
 if number >10 and color == "blue":
     print()
 if number <=10 and color !="blue":
     print()
-
-#if number >10 or color == "blue":
-    #print("the test is true")
-#if number <10 and color =="blue":
-    #print("the test is True")
-
