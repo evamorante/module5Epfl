529cd7dd70c900937f1c49a7f41609b36f7c9600
EvaDM <evamorante1969@gmail.com>
2023-04-17 10:54:53 +0200

    html file linked with the last execise in js (guessGame)

--- TheGNGWithinFunction.js
+++ TheGNGWithinFunction.js
@@ -1,21 +1,25 @@
-function guess_number(nombre, couleur){
-    
-    nombreQuestion = windows.prompt("Devinez le nombre magique : ");
-    couleurQuestion = windows.prompt("Découvrez la couleur magique : ");
+function guessGame (){
+    var nombre = 24
+    var couleur = "vert"
+    var nombreQuestion = window.prompt("Devinez le nombre magique : ");
+    var couleurQuestion = window.prompt("Découvrez la couleur magique : ");
 
     if (nombre == Number(nombreQuestion) && couleur == couleurQuestion){
         console.log("Bravo, vous avez trouvé le nombre et la couleur magiques !");
     }
-    else if (nombre == Number(nombre_question) || couleur == couleur_question){
+    else if (nombre == Number(nombreQuestion) || couleur == couleurQuestion){
         console.log("Bravo, vous avez trouvé un des deux mystères !");
     }
     else{
-        print("Vous n'avez trouvé aucun des deux mystères");
-        print("Bonne chance pour la prochaine !");
+        console.log("Vous n'avez trouvé aucun des deux mystères");
+        console.log("Bonne chance pour la prochaine !");
     }
         
     console.log("Essayez à nouveau");
 }
 
-    
-console.log(guess_number(24, "vert"));
+guessGame();
+
+//it works !!! youhrrraaaa !
+//si je pose les deux variables dans les paramètres de la fonction, et que je les pose lorsque j'appelle
+//la fonction, ça joue également, toutefois j'ai une ligne "undiefined" en grisé en plus en dessous des outputs
