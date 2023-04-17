0ad6300f6b4261fe4596f70ecf4a9f0a82024ac6
EvaDM <evamorante1969@gmail.com>
2023-04-17 15:36:56 +0200

    numéro d'exercice corrigé

--- And&OrExercise.js
+++ And&OrExercise.js
@@ -1,7 +1,8 @@
-/**#If the language is Python or JavaScript, it is a good course.
-#If the languages are Python and JavaScript, this is the Thinking and Creating with Code course.
-#If the number is bigger than 10 or the color is blue, the test is true.
-#If the number is not bigger than 10 and the color is not blue, the test is true.*/
+//exercice 30. module 5
+/**If the language is Python or JavaScript, it is a good course.
+If the languages are Python and JavaScript, this is the Thinking and Creating with Code course.
+If the number is bigger than 10 or the color is blue, the test is true.
+If the number is not bigger than 10 and the color is not blue, the test is true.*/
 
 var python = true
 var javascript = true
